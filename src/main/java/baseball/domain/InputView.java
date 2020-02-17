b8e90a9370691ec5e85b91bdb4ff1cbfe5b16b22
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-02-17 10:59:04 +0900

    fix : 입력값 return datatype 수정
    - parseInt를 baseBall에서 하도록 수정함에 따라 return type을 int -> string으로 수정

--- src/main/java/baseball/domain/InputView.java
+++ src/main/java/baseball/domain/InputView.java
@@ -3,9 +3,9 @@ package baseball.domain;
 import java.util.Scanner;
 
 public class InputView {
-    public static int getInput(){
+    public static String getInput(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("서로 다른 수로 이루어진 3자리의 수를 입력해주세요.");
-        return Integer.parseInt(scanner.nextLine());
+        return scanner.nextLine();
     }
 }
